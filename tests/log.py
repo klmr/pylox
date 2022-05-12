2b21345b90e7c0eed472b4e4ea1713421acade6c
Konrad Rudolph <konrad.rudolph@gmail.com>
2022-05-12 23:53:44 +0100

    Fix test mock logger

--- tests/log.py
+++ tests/log.py
@@ -3,16 +3,15 @@ from klmr.pylox.token import Token
 
 
 class MockLogger(Logger):
-    had_error = False
-
     def reset(self, source: str) -> None:
         pass
 
     def scan_error(self, position: tuple[int, int], message: str) -> None:
-        raise ValueError(message)
+        row, col = position
+        raise ValueError(f'at ({row}, {col}): {message}')
 
-    def parse_error(self, position: tuple[int, int], token: Token, message: str) -> None:
-        raise ValueError(message)
+    def parse_error(self, token: Token, message: str) -> None:
+        raise ValueError(f'at {token.lexeme}: {message}')
 
     def runtime_error(self, error: LoxRuntimeError) -> None:
         raise error
